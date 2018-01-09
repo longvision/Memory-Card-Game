8dbe03fed89b52dd7b21cfea89c834dd685cf127
Ricardo Naoki Horiguchi <r.n.hori@gmail.com>
2018-01-09 09:54:34 -0200

    deck diplay amended.Cards not yet turning.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
 /*
  * Create a list that holds all of your cards
  */
-
+var Cards = ["fa fa-diamond","fa fa-diamond","fa fa-paper-plane-o","fa fa-paper-plane-o",
+"fa fa-anchor","fa fa-anchor","fa fa-bolt","fa fa-bolt","fa fa-cube","fa fa-cube",
+"fa fa-leaf","fa fa-leaf","fa fa-bicycle","fa fa-bicycle","fa fa-bomb","fa fa-bomb"];
+//Stores the array of open cards turned on the table.
+var openCards=[];
+//Stores the shuffled cards when game begins.
+var shuffledDeck=[];
+var cardsUp=0;
+var movesCount=0;
 
 /*
  * Display the cards on the page
@@ -24,10 +32,28 @@ function shuffle(array) {
 
     return array;
 }
+//shuffle the cards when game starts.
+
+shuffledDeck=shuffle(Cards);
 
 
+// HTML elements creation for deck and cards
+
+    $('.container').append('<ul class="deck"></ul>');
+    for (var i = 0;i < Cards.length;i++){
+      $('.deck').prepend('<li class="card"></li>');
+    }
+    $('.card').prepend('<i></i>');
+    for (var i = 0;i < Cards.length;i++){
+      $('.card').eq(i).find('i').addClass(shuffledDeck[i]);
+    }
+
+
+//Restart the game and reload the table.
+$('.restart').click(function(){
+  location.reload();
+ });
 /*
- * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
@@ -36,3 +62,43 @@ function shuffle(array) {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+
+
+  $('.deck').on('click','.card', function(event){
+  /*Essential condition for flipping a card i.e. a card may be flipped only if class='card' is present which is true always and only one other card is open.*/
+    if($(this).attr('class')==='card' && openCards.length<2){
+    //Class name of a open card is pushed to opencards array when only one card is opened.
+      if (openCards.length===0){
+        $(this).toggleClass('open');
+        openCards.push($(this).children().attr('class'));
+      }
+      else if(openCards.length===1){
+        $(this).toggleClass('open');
+        openCards.push($(this).children().attr('class'));
+
+      //Comparison of two open cards-If matched
+        if (openCards[0]===openCards[1]){
+            $('.card').filter($('.open')).toggleClass('open match');
+            //Increases the number os cards turned up by two in the list.
+            cardsUp=cardsUp+2;
+            //Increases the number of movement made after clicked on a card.
+            movesCount=movesCount+1;
+            $('.moves').text(movesCount);
+            //Clears the comparison array list.
+            openCards=[];
+        }
+        else{
+          function flipBack(){
+          $('.card').filter($('.open')).toggleClass('open');
+          openCards = [];
+          movesCount=movesCount+1;
+          $('.moves').text(movesCount);
+          }
+        setTimeout(flipBack, 600);
+        }
+      }
+    }
+  });
